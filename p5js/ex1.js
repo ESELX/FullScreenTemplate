cfe3f1dafc7284451a2c428c1c13f50fb27d5f5d
André Rocha <steam228@gmail.com>
2019-12-18 12:16:27 +0000

    TEMPLATEOK

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -2,14 +2,7 @@
 
 function setup() { //código que só corre uma vez
 
-  ///Não Apagar para Baixo até <<<
-  var canvasDiv = document.getElementById('ex1');
-  var width = canvasDiv.offsetWidth;
-  var sketchCanvas = createCanvas(width,width*.75);
-  console.log(sketchCanvas);
-  sketchCanvas.parent('ex1');
-  ///<<< Não fica a faltar createCanvas
-
+createCanvas(windowWidth,windowHeight);
 
 }
 
